0566cdca540b06038defd25c3230dc1907a66975
Skyler Mäntysaari <sm+git@skym.fi>
2025-01-02 07:03:07 +0200

    Should be fine?

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -47,8 +47,8 @@ resource "aws_launch_template" "amd64" {
     bare_metal = "excluded"
 
     vcpu_count {
-      min = 4
-      max = 16
+      min = 8
+      max = 24
     }
 
     memory_mib {
@@ -102,7 +102,7 @@ resource "aws_launch_template" "arm64" {
 
     vcpu_count {
       min = 8
-      max = 16
+      max = 24
     }
 
     memory_mib {
